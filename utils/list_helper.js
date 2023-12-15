b62fd530098860e1a3b52d00e3a76c0dc7d73a2f
David <davidgonzalezj97@gmail.com>
2023-12-15 19:12:50 +0100

    Exercise 4.5 Making and passing tests for favoriteBlog()

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,13 +3,31 @@ const dummy = (blogs) => {
 }
 
 const totalLikes = (blogs) => {
-    var total = 0
+    let total = 0
     blogs.map((blog)=>{
         total += blog.likes
     })
     return total
 }
 
+const favoriteBlog = (blogs) => {
+    let mostLiked = {}
+    let mostLikes = 0
+
+    blogs.map((blog)=>{
+        if(blog.likes > mostLikes){
+            mostLikes = blog.likes
+            mostLiked = {
+                'title': blog.title,
+                'author': blog.author,
+                'likes': blog.likes
+            }
+        }
+    })
+
+    return mostLiked
+}
+
 module.exports = {
-    dummy, totalLikes
+    dummy, totalLikes, favoriteBlog
 }
