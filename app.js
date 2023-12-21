f681c9ab543be10ab0756350ed91aef54527e5b7
David <davidgonzalezj97@gmail.com>
2023-12-21 21:56:27 +0100

    Exercise 4.20 Refactoring the token extractor into a middleware that gets the token into the request

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
 const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
 require('dotenv').config()
 
 app.use(express.json())
@@ -15,24 +16,15 @@ mongoose.connect(MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
+
+app.use(middleware.tokenExtractor)
+
 app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 
-//Error handling Middleware
-const errorHandler = (error, request, response, next) => {
-  
-    if (error.name === 'CastError') {
-      return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-      return response.status(400).json({ error: error.message })
-    } else if (error.name === 'JsonWebTokenError'){
-      return response.status(401).json({ error: error.message })
-    }
-  
-    next(error)
-}
 
-app.use(errorHandler)
+
+app.use(middleware.errorHandler)
 
 module.exports = app
